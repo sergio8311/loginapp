5e7754ca1cc4ca48371fb36aee900fb835293995
sergii <sergey8311@gmail.com>
2017-10-16 21:22:22 +0300

    add register routes var

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,15 @@ router.get('/login', function (req, res) {
     res.render('login');
 })
 
+//Register User
+router.post('/register', function (req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+
+    console.log(name);
+})
+
 module.exports = router;
